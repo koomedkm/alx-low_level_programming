5c0a4edf14e65e085c1b8cfd0ff99e06ff379e65
koomedkm <deniskoome1@gmail.com>
2022-10-18 00:47:02 +0300

    initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*-C program that prints exactly "Programming is like building a multilingual puzzle 
+*/
+
+int main (void)
+    {
+    puts ("\"Programming is like building a multilingual puzzle");
+    
+    return 0;
+    }
