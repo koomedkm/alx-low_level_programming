8e71a929f140aa176b94e4ab0d6402a0bcf1c6bc
koomedkm <deniskoome1@gmail.com>
2022-10-22 13:45:17 +0300

    print small case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main-print s alphabet in lower case
+  *followed by newline
+  *Return Always  (success)
+  */
+int main(void)
+{
+	char ch;
+
+	for (ch 'a'; ch <= 'z'; ch++)
+		{
+			putchar (ch);
+		}
+			putchar('\n');
+	return (0);
+}
