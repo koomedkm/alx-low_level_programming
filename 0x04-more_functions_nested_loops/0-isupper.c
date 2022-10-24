173666121da42013a3efb92ab8ada114e58bbcb9
koomedkm <deniskoome1@gmail.com>
2022-10-24 23:54:36 +0300

    initial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  *-isupper  - function that checks for uppercase character 
+  *variable type int
+  *Return 1 for uppwercase and 0 otherwise
+  */
+int _isupper(int c);
+{
+	if (c >=65 && <= 90 )
+	{
+	return (1);
+	}
+	else 
+	{
+	return (0);
+		}
+}
+    
