0f341bf20352858a191b5a774e19c8a06e8d8255
koomedkm <deniskoome1@gmail.com>
2023-02-17 01:28:59 +0300

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,5 +5,7 @@
  *Return 1 (success)
  */
 int main(void)
-{printf(and that piece of art is useful\" - Dora Korpar, 2015-10-19)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
 }
