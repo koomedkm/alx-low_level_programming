fdbf73fbde8684d05cedbf9b815159296c5c3374
koomedkm <deniskoome1@gmail.com>
2023-02-17 01:19:49 +0300

    initial commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ * prints a statement
+ *Return 1 (success)
+ */
+int main(void)
+{printf(and that piece of art is useful\" - Dora Korpar, 2015-10-19)
+}
