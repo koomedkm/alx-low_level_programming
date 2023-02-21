5d39e4e60d4cedcf9bbe3d2cb8774e38f139db14
koomedkm <deniskoome1@gmail.com>
2023-02-22 01:18:54 +0300

    add file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<main.h>
+/**
+ *main- checks the code
+ *prints alphabet in lower case
+ */Return: Always 0
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a' ; j <= z ; j++)
+	{
+	_putchar (j);
+	}
+		_putchar ('\n');
+}
+
