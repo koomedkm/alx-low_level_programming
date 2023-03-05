9153136ae587a24f6072957abcd31da702e92fe9
koomedkm <deniskoome1@gmail.com>
2023-03-06 01:07:43 +0300

    add file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *strcat - adding two lines together
+ *@dest: first word
+ *@src: second word
+ *Return: dest
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; src[j] != '\0'; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+	return (dest);
+}
+
